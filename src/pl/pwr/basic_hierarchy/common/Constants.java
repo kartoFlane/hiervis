ec2614d49289a79fdcdb6a9886bc2c8a4e7b7d3e
kartoFlane <tbachminski@gmail.com>
2016-10-28 16:03:15 +0200

    Removed unused constants from basic_hierarchy Constants class

--- src/pl/pwr/basic_hierarchy/common/Constants.java
+++ src/pl/pwr/basic_hierarchy/common/Constants.java
@@ -6,8 +6,4 @@ public class Constants
 	public static final String HIERARCHY_BRANCH_SEPARATOR = ".";
 	public static final String HIERARCHY_BRANCH_SEPARATOR_REGEX = "\\.";
 	public static final String ROOT_ID = "gen.0";
-	public static final String NODES_PREFIX = "gen";
-	public static final String ARTIFICIAL_ROOT = "ARTIFICIAL_ROOT";
-	public static final int INDEX_OF_GROUND_TRUTH_IN_WEKA_INSTANCE = 1;
-	public static final int INDEX_OF_ASSIGN_CLASS_IN_WEKA_INSTANCE = 0;
 }
