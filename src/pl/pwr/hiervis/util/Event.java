509792610f859d503873344d33b3308f84aadae9
kartoFlane <tbachminski@gmail.com>
2017-01-03 23:56:38 +0100

    Expanded documentation for Event. Added logging to help detect removeListner pitfall.
    - addListener() now returns the listener passed in argument, so that its more flexible in how the listener is saved to a variable in client code.

--- src/pl/pwr/hiervis/util/Event.java
+++ src/pl/pwr/hiervis/util/Event.java
@@ -4,6 +4,9 @@ import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.function.Consumer;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 /**
  * This class is meant to emulate C#-like events in syntax, in order to avoid Java's listener
@@ -14,6 +17,8 @@ import java.util.function.Consumer;
  * therefore anyone can invoke the handler and broadcast events to listeners.
  * This lack of fine access control makes this class unusable for anything other than private projects.
  * </p>
+ * 
+ * <h1>Thread Safety</h1>
  * <p>
  * Theoretically this class should be thread-safe, but this has not been tested at all.
  * </p>
@@ -24,6 +29,24 @@ import java.util.function.Consumer;
  * to avoid potential threading issues.
  * </p>
  * 
+ * <h1>Java 8 Lambdas and Method References</h1>
+ * <p>
+ * Using this class in conjunction with Java 8 method references requires one to be aware
+ * that each method reference ({@code this::example}) creates a new closure. Despite referencing the same
+ * method, these closures are not only different, but also impossible to compare. Both {@code ==} and
+ * {@code equals()} will return false.
+ * As such, it's not possible to do something like this:
+ * </p>
+ * <p>
+ * <code>
+ * event.removeListener( this::onEvent )
+ * </code>
+ * </p>
+ * <p>
+ * ...as much as I'd want to. The listener has to be saved to a variable when registered, and then
+ * unregistered by passing that variable into {@link #removeListener(Consumer)}.
+ * </p>
+ * 
  * @author Tomasz Bachmiński
  *
  * @param <T>
@@ -31,25 +54,33 @@ import java.util.function.Consumer;
  */
 public class Event<T>
 {
+	private static final Logger log = LogManager.getLogger( Event.class );
+
 	private Set<Consumer<? super T>> listeners = null;
 
 
-	public void addListener( Consumer<? super T> listener )
+	public Consumer<? super T> addListener( Consumer<? super T> listener )
 	{
 		if ( listener == null )
-			return;
+			throw new IllegalArgumentException( "Argument must not be null." );
 		if ( listeners == null )
 			listeners = new CopyOnWriteArraySet<>();
 		listeners.add( listener );
+		return listener;
 	}
 
 	public void removeListener( Consumer<? super T> listener )
 	{
 		if ( listener == null )
-			return;
+			throw new IllegalArgumentException( "Argument must not be null." );
 		if ( listeners == null || listeners.size() == 0 )
 			return;
-		listeners.remove( listener );
+		if ( !listeners.remove( listener ) ) {
+			log.trace(
+				"Tried to remove a listener that was not registered.\n",
+				new Exception( "Stack trace" )
+			);
+		}
 	}
 
 	public void clearListeners()
